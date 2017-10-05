759d47e3a6e74a6459547790f1b4d273ec8405f3
Kevin Martzolf <kmart914@gmail.com>
2017-10-05 11:27:11 -0700

    updated controller with sequelize routes

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -1,39 +1,37 @@
 var express = require("express");
+var router = express.router();
+var db = require ("../models");
 
-var router = express.Router();
-var burger = require("../models/burger");
-
-// get route -> index
 router.get("/", function(req, res) {
   res.redirect("/burgers");
 });
 
 router.get("/burgers", function(req, res) {
-  // express callback response by calling burger.selectAllBurger
-  burger.all(function(data) {
-    // Wrapping the array of returned burgers in a object so it can be referenced inside our handlebars
-    var hbsObject = { burgers: data };
-    res.render("index", hbsObject);
+  db.Burger.findAll({}).then(function(data) {
+    var hbsObj = {
+      burgers: data
+    };
+    res.render("index", hbsObj);
   });
 });
 
-// post route -> back to index
 router.post("/burgers/create", function(req, res) {
-  // takes the request object using it as input for buger.addBurger
-  burger.create(req.body.burger_name, function(result) {
-    // wrapper for orm.js that using MySQL insert callback will return a log to console,
-    // render back to index with handle
-    console.log(result);
+  db.Burger.create({
+    burger_name: req.body.burger_name,
+    devoured: false
+  }).then(function(data) {
     res.redirect("/");
   });
 });
 
-// put route -> back to index
 router.put("/burgers/update", function(req, res) {
-  burger.update(req.body.burger_id, function(result) {
-    // wrapper for orm.js that using MySQL update callback will return a log to console,
-    // render back to index with handle
-    console.log(result);
+  db.Burger.update({
+    devoured: true
+  }, {
+    where: {
+      id: req.params.burger_id
+    }
+  }).then(function(data) {
     res.redirect("/");
   });
 });
